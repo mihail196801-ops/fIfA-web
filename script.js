d23275a8cd9ff772043868aafb083645a4b37e66
mihail196801-ops <mihail1968.01@gmail.com>
2025-11-10 16:14:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,34 +26,39 @@ let isPenaltyShootout = false;
 let attempts = 0;
 let points = 0;
 let playerBalance = 1000000; // Баланс игрока (в монетах)
+let quests = [
+  { id: 'quest1', name: 'Забей 5 голов', reward: 50000, completed: false },
+  { id: 'quest2', name: 'Сделай 10 пасов', reward: 30000, completed: false },
+  { id: 'quest3', name: 'Выиграй матч', reward: 100000, completed: false }
+];
 
 // Список игроков с их характеристиками и позициями
 const playerData = [
-  // Команда 1 (домашняя)
-  { id: 'ronaldo', name: 'Cristiano Ronaldo', rating: 97, position: 'ST', pac: 93, sho: 99, pas: 90, dri: 94, def: 75, phy: 99, team: 'Al Nassr', nationality: 'Portugal', model: 'models/player1.glb', price: 1800000 },
-  { id: 'messi', name: 'Lionel Messi', rating: 97, position: 'CAM', pac: 94, sho: 92, pas: 97, dri: 98, def: 80, phy: 87, team: 'Inter Miami', nationality: 'Argentina', model: 'models/player2.glb', price: 1600000 },
-  { id: 'kane', name: 'Kane', rating: 98, position: 'CM', pac: 93, sho: 96, pas: 98, dri: 94, def: 92, phy: 93, team: 'Bayern Munich', nationality: 'England', model: 'models/player1.glb', price: 1900000 },
-  { id: 'griezmann', name: 'Griezmann', rating: 98, position: 'CAM', pac: 92, sho: 94, pas: 97, dri: 98, def: 80, phy: 87, team: 'Atlético Madrid', nationality: 'France', model: 'models/player2.glb', price: 1500000 },
-  { id: 'konate', name: 'Konaté', rating: 98, position: 'GK', pac: 98, sho: 97, pas: 95, dri: 94, def: 85, phy: 94, team: 'Liverpool', nationality: 'France', model: 'models/goalkeeper.glb', price: 400000 },
-  { id: 'rodri', name: 'Rodri', rating: 98, position: 'CB', pac: 93, sho: 80, pas: 96, dri: 93, def: 98, phy: 95, team: 'Man City', nationality: 'Spain', model: 'models/player1.glb', price: 1700000 },
-  { id: 'mcTominay', name: 'McTominay', rating: 98, position: 'ST', pac: 93, sho: 99, pas: 90, dri: 94, def: 75, phy: 99, team: 'Man Utd', nationality: 'Scotland', model: 'models/player2.glb', price: 300000 },
-  { id: 'hansen', name: 'Hansen', rating: 97, position: 'RW', pac: 96, sho: 95, pas: 95, dri: 97, def: 58, phy: 86, team: 'Chelsea', nationality: 'Denmark', model: 'models/player1.glb', price: 400000 },
-  { id: 'viniJr', name: 'Vini Jr.', rating: 97, position: 'LW', pac: 99, sho: 91, pas: 90, dri: 98, def: 38, phy: 78, team: 'Real Madrid', nationality: 'Brazil', model: 'models/player2.glb', price: 1200000 },
-  { id: 'salah', name: 'Salah', rating: 97, position: 'RW', pac: 97, sho: 95, pas: 92, dri: 97, def: 60, phy: 85, team: 'Liverpool', nationality: 'Egypt', model: 'models/player1.glb', price: 700000 },
-  { id: 'pelé', name: 'Pelé', rating: 97, position: 'CAM', pac: 95, sho: 97, pas: 93, dri: 96, def: 61, phy: 82, team: 'Santos', nationality: 'Brazil', model: 'models/player2.glb', price: 2300000 },
+  // Команда 1 (домашняя) - красный цвет
+  { id: 'ronaldo', name: 'Cristiano Ronaldo', rating: 97, position: 'ST', pac: 93, sho: 99, pas: 90, dri: 94, def: 75, phy: 99, team: 'Al Nassr', nationality: 'Portugal', model: 'models/player1.glb', price: 1800000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p184758707.png?fm=png&ixlib=java-2.1.0&verzion=2&w=100&s=ff8bfad1cf1fa5876da738ff69749703' },
+  { id: 'messi', name: 'Lionel Messi', rating: 97, position: 'CAM', pac: 94, sho: 92, pas: 97, dri: 98, def: 80, phy: 87, team: 'Inter Miami', nationality: 'Argentina', model: 'models/player2.glb', price: 1600000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p134470099.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=a2af0c196cda265978c6316532b0e7c2' },
+  { id: 'kane', name: 'Kane', rating: 98, position: 'CM', pac: 93, sho: 96, pas: 98, dri: 94, def: 92, phy: 93, team: 'Bayern Munich', nationality: 'England', model: 'models/player1.glb', price: 1900000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p201528718.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=3b3f15d18d4efd7d25aee4f0a6ffa9ce' },
+  { id: 'griezmann', name: 'Griezmann', rating: 98, position: 'CAM', pac: 92, sho: 94, pas: 97, dri: 98, def: 80, phy: 87, team: 'Atlético Madrid', nationality: 'France', model: 'models/player2.glb', price: 1500000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p151189709.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=c9237669d27d934cfdbdb3ede5e9a48a' },
+  { id: 'konate', name: 'Konaté', rating: 98, position: 'GK', pac: 98, sho: 97, pas: 95, dri: 94, def: 85, phy: 94, team: 'Liverpool', nationality: 'France', model: 'models/goalkeeper.glb', price: 400000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p84123758.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=dd74d4fdfc9aec8e4f69b85e5bacb65d' },
+  { id: 'rodri', name: 'Rodri', rating: 98, position: 'CB', pac: 93, sho: 80, pas: 96, dri: 93, def: 98, phy: 95, team: 'Man City', nationality: 'Spain', model: 'models/player1.glb', price: 1700000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p100895162.png?fm=png&ixlib=java-2.1.0&w=100&s=aa9907356943a4ebbd9562d56ea2d902' },
+  { id: 'mcTominay', name: 'McTominay', rating: 98, position: 'ST', pac: 93, sho: 99, pas: 90, dri: 94, def: 75, phy: 99, team: 'Man Utd', nationality: 'Scotland', model: 'models/player2.glb', price: 300000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p134454966.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=3ad0b92e50457218de6b767ec7f42927' },
+  { id: 'hansen', name: 'Hansen', rating: 97, position: 'RW', pac: 96, sho: 95, pas: 95, dri: 97, def: 58, phy: 86, team: 'Chelsea', nationality: 'Denmark', model: 'models/player1.glb', price: 400000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p117667614.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=13e5c4cdaea9fecc5140e7b870f4f47d' },
+  { id: 'viniJr', name: 'Vini Jr.', rating: 97, position: 'LW', pac: 99, sho: 91, pas: 90, dri: 98, def: 38, phy: 78, team: 'Real Madrid', nationality: 'Brazil', model: 'models/player2.glb', price: 1200000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p151233738.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=f8c64d1a5945b0b88248bb5cfbdda9d2' },
+  { id: 'salah', name: 'Salah', rating: 97, position: 'RW', pac: 97, sho: 95, pas: 92, dri: 97, def: 60, phy: 85, team: 'Liverpool', nationality: 'Egypt', model: 'models/player1.glb', price: 700000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p184758707.png?fm=png&ixlib=java-2.1.0&verzion=2&w=100&s=ff8bfad1cf1fa5876da738ff69749703' },
+  { id: 'pelé', name: 'Pelé', rating: 97, position: 'CAM', pac: 95, sho: 97, pas: 93, dri: 96, def: 61, phy: 82, team: 'Santos', nationality: 'Brazil', model: 'models/player2.glb', price: 2300000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p67345931.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=b0886397ad875c66853acc4946487382' },
 
-  // Команда 2 (Гостевая)
-  { id: 'mbappe', name: 'Mbappé', rating: 91, position: 'ST', pac: 97, sho: 90, pas: 80, dri: 92, def: 36, phy: 78, team: 'Real Madrid', nationality: 'France', model: 'models/player1.glb', price: 1300000 },
-  { id: 'dembele', name: 'Dembélé', rating: 97, position: 'RW', pac: 99, sho: 90, pas: 94, dri: 97, def: 55, phy: 75, team: 'Barcelona', nationality: 'France', model: 'models/player2.glb', price: 1300000 },
-  { id: 'cruyff', name: 'Cruyff', rating: 98, position: 'LM', pac: 96, sho: 92, pas: 98, dri: 99, def: 80, phy: 86, team: 'Ajax', nationality: 'Netherlands', model: 'models/player1.glb', price: 4200000 },
-  { id: 'maradona', name: 'Maradona', rating: 98, position: 'RW', pac: 94, sho: 94, pas: 97, dri: 99, def: 45, phy: 85, team: 'Napoli', nationality: 'Argentina', model: 'models/player2.glb', price: 2700000 },
-  { id: 'vanDijk', name: 'van Dijk', rating: 97, position: 'CB', pac: 89, sho: 68, pas: 81, dri: 80, def: 97, phy: 96, team: 'Liverpool', nationality: 'Netherlands', model: 'models/player1.glb', price: 1900000 },
-  { id: 'tah', name: 'Tah', rating: 97, position: 'CB', pac: 90, sho: 70, pas: 83, dri: 85, def: 97, phy: 95, team: 'Bayer Leverkusen', nationality: 'Germany', model: 'models/player2.glb', price: 270000 },
-  { id: 'bastoni', name: 'Bastoni', rating: 97, position: 'CB', pac: 89, sho: 55, pas: 87, dri: 90, def: 97, phy: 96, team: 'Inter Milan', nationality: 'Italy', model: 'models/player1.glb', price: 260000 },
-  { id: 'osimhen', name: 'Osimhen', rating: 97, position: 'ST', pac: 97, sho: 97, pas: 81, dri: 93, def: 53, phy: 92, team: 'Napoli', nationality: 'Nigeria', model: 'models/player2.glb', price: 280000 },
-  { id: 'wilson', name: 'Wilson', rating: 97, position: 'ST', pac: 98, sho: 95, pas: 87, dri: 98, def: 54, phy: 93, team: 'Arsenal', nationality: 'England', model: 'models/player1.glb', price: 390000 },
-  { id: 'retgui', name: 'Retegui', rating: 97, position: 'ST', pac: 96, sho: 97, pas: 89, dri: 95, def: 60, phy: 94, team: 'Juventus', nationality: 'Italy', model: 'models/player2.glb', price: 350000 },
-  { id: 'shaw', name: 'Shaw', rating: 97, position: 'ST', pac: 94, sho: 97, pas: 86, dri: 93, def: 43, phy: 96, team: 'Man Utd', nationality: 'England', model: 'models/player1.glb', price: 430000 }
+  // Команда 2 (Гостевая) - синий цвет
+  { id: 'mbappe', name: 'Mbappé', rating: 91, position: 'ST', pac: 97, sho: 90, pas: 80, dri: 92, def: 36, phy: 78, team: 'Real Madrid', nationality: 'France', model: 'models/player1.glb', price: 1300000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p151233738.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=f8c64d1a5945b0b88248bb5cfbdda9d2' },
+  { id: 'dembele', name: 'Dembélé', rating: 97, position: 'RW', pac: 99, sho: 90, pas: 94, dri: 97, def: 55, phy: 75, team: 'Barcelona', nationality: 'France', model: 'models/player2.glb', price: 1300000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p151226387.png?fm=png&ixlib=java-2.1.0&verzion=2&w=100&s=34fecc4cd5fe05825cc8debee8c65735' },
+  { id: 'cruyff', name: 'Cruyff', rating: 98, position: 'LM', pac: 96, sho: 92, pas: 98, dri: 99, def: 80, phy: 86, team: 'Ajax', nationality: 'Netherlands', model: 'models/player1.glb', price: 4200000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p84076125.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=842a17dcdcb173a4725f9bccdee1b5a5' },
+  { id: 'maradona', name: 'Maradona', rating: 98, position: 'RW', pac: 94, sho: 94, pas: 97, dri: 99, def: 45, phy: 85, team: 'Napoli', nationality: 'Argentina', model: 'models/player2.glb', price: 2700000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p201516634.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=53077f92c06616402396bb917ea8c9ed' },
+  { id: 'vanDijk', name: 'van Dijk', rating: 97, position: 'CB', pac: 89, sho: 68, pas: 81, dri: 80, def: 97, phy: 96, team: 'Liverpool', nationality: 'Netherlands', model: 'models/player1.glb', price: 1900000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p117643888.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=c55459ae566e913edeb4f697f20938c7' },
+  { id: 'tah', name: 'Tah', rating: 97, position: 'CB', pac: 90, sho: 70, pas: 83, dri: 85, def: 97, phy: 95, team: 'Bayer Leverkusen', nationality: 'Germany', model: 'models/player2.glb', price: 270000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p117653843.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=88367289084962fd87ca4d73464d1adf' },
+  { id: 'bastoni', name: 'Bastoni', rating: 97, position: 'CB', pac: 89, sho: 55, pas: 87, dri: 90, def: 97, phy: 96, team: 'Inter Milan', nationality: 'Italy', model: 'models/player1.glb', price: 260000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p84123463.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=988fb90ef66abd978e99c6b3019c2cbd' },
+  { id: 'osimhen', name: 'Osimhen', rating: 97, position: 'ST', pac: 97, sho: 97, pas: 81, dri: 93, def: 53, phy: 92, team: 'Napoli', nationality: 'Nigeria', model: 'models/player2.glb', price: 280000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p117672805.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=112efdc268fa9be33212f948fe34a417' },
+  { id: 'wilson', name: 'Wilson', rating: 97, position: 'ST', pac: 98, sho: 95, pas: 87, dri: 98, def: 54, phy: 93, team: 'Arsenal', nationality: 'England', model: 'models/player1.glb', price: 390000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p100927308.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=6e74c0a202dbc73c3023566c5d5646a5' },
+  { id: 'retgui', name: 'Retegui', rating: 97, position: 'ST', pac: 96, sho: 97, pas: 89, dri: 95, def: 60, phy: 94, team: 'Juventus', nationality: 'Italy', model: 'models/player2.glb', price: 350000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p117682362.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=75d26deafe25fc91c82deef48e6aab91' },
+  { id: 'shaw', name: 'Shaw', rating: 97, position: 'ST', pac: 94, sho: 97, pas: 86, dri: 93, def: 43, phy: 96, team: 'Man Utd', nationality: 'England', model: 'models/player1.glb', price: 430000, avatar: 'https://cdn3.futbin.com/content/fifa25/img/players/p134463947.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=82823ce25d9b7ffff57737e554f40391' }
 ];
 
 // Стартовые позиции игроков (в метрах, относительно центра поля)
@@ -92,6 +97,14 @@ let keys = {
   s: false,
   d: false
 };
+
+// Удары
+let isShooting = false;
+let shootStartPos = null;
+let shootEndPos = null;
+
+// Камера
+let orbitControls = null;
 
 // =================================
 // ИНИЦИАЛИЗАЦИЯ
@@ -302,11 +315,66 @@ function setupEventListeners() {
         if (e.key === 'd') keys.d = false;
     });
 
+    // Удары
+    document.addEventListener('mousedown', (e) => {
+      if (e.button === 0) { // ЛКМ
+        isShooting = true;
+        shootStartPos = getMousePosition(e);
+      }
+    });
+
+    document.addEventListener('mouseup', (e) => {
+      if (e.button === 0) { // ЛКМ
+        isShooting = false;
+        shootEndPos = getMousePosition(e);
+        if (shootStartPos && shootEndPos) {
+          performShot(shootStartPos, shootEndPos);
+        }
+        shootStartPos = null;
+        shootEndPos = null;
+      }
+    });
+
+    // Камера
+    orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+    orbitControls.enableDamping = true;
+    orbitControls.dampingFactor = 0.25;
+    orbitControls.enableZoom = true;
+
     // Кнопки UI (создаем динамически)
     createButtons();
 
     // Джойстик — убираем, так как управляем WASD
     // document.getElementById('joystick').remove();
+}
+
+function getMousePosition(event) {
+  const rect = renderer.domElement.getBoundingClientRect();
+  return {
+    x: ((event.clientX - rect.left) / rect.width) * 2 - 1,
+    y: -((event.clientY - rect.top) / rect.height) * 2 + 1
+  };
+}
+
+function performShot(startPos, endPos) {
+  if (!isPlaying) return;
+  attempts++;
+
+  // Расчет направления удара
+  const direction = new THREE.Vector3(endPos.x - startPos.x, 0, endPos.y - startPos.y);
+  direction.normalize();
+
+  // Прикладываем импульс к мячу
+  const force = 10; // Сила удара
+  ballBody.applyImpulse(new CANNON.Vec3(direction.x * force, 0, direction.z * force), ballBody.position);
+
+  // Шанс забить
+  if (Math.random() > 0.3) { // 70% шанс
+    scoreGoal();
+  } else {
+    playWhistleSound(); // Промах
+  }
+  updateHUD();
 }
 
 function createButtons() {
@@ -423,6 +491,12 @@ function showMarket() {
   loadMarket();
 }
 
+function showQuests() {
+  document.getElementById('homeScreen').classList.remove('active');
+  document.getElementById('questsScreen').classList.add('active');
+  loadQuests();
+}
+
 function resetGame() {
   scoreHome = 0;
   scoreAway = 0;
@@ -536,16 +610,7 @@ function loadPlayerCards() {
 
     // Используем изображение с Futbin
     const img = document.createElement('img');
-    let futbinId = 'p184758707'; // Роналду по умолчанию
-    if (data.id === 'griezmann') futbinId = 'p151189709';
-    if (data.id === 'konate') futbinId = 'p84123758';
-    if (data.id === 'kane') futbinId = 'p201528718';
-    if (data.id === 'messi') futbinId = 'p134470099';
-    if (data.id === 'mbappe') futbinId = 'p151233738'; // Mbappé
-    if (data.id === 'salah') futbinId = 'p184758707'; // Salah
-    if (data.id === 'vanDijk') futbinId = 'p117643888'; // van Dijk
-
-    img.src = `https://cdn3.futbin.com/content/fifa25/img/players/${futbinId}.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=12345`;
+    img.src = data.avatar;
     img.alt = data.name;
     img.onerror = function() { this.src = 'images/cards/generic_player.png'; };
 
@@ -599,16 +664,7 @@ function loadMarket() {
 
     // Изображение игрока
     const img = document.createElement('img');
-    let futbinId = 'p184758707'; // Роналду по умолчанию
-    if (data.id === 'griezmann') futbinId = 'p151189709';
-    if (data.id === 'konate') futbinId = 'p84123758';
-    if (data.id === 'kane') futbinId = 'p201528718';
-    if (data.id === 'messi') futbinId = 'p134470099';
-    if (data.id === 'mbappe') futbinId = 'p151233738'; // Mbappé
-    if (data.id === 'salah') futbinId = 'p184758707'; // Salah
-    if (data.id === 'vanDijk') futbinId = 'p117643888'; // van Dijk
-
-    img.src = `https://cdn3.futbin.com/content/fifa25/img/players/${futbinId}.png?fm=png&ixlib=java-2.1.0&verzion=1&w=100&s=12345`;
+    img.src = data.avatar;
     img.alt = data.name;
     img.onerror = function() { this.src = 'images/cards/generic_player.png'; };
 
@@ -644,6 +700,49 @@ function loadMarket() {
     marketCard.appendChild(buyBtn);
 
     container.appendChild(marketCard);
+  }
+}
+
+// =================================
+// КВЕСТЫ
+// =================================
+
+function loadQuests() {
+  const container = document.querySelector('.quests-container');
+  container.innerHTML = '';
+
+  for (const quest of quests) {
+    const questCard = document.createElement('div');
+    questCard.className = 'quest-card';
+
+    const name = document.createElement('div');
+    name.className = 'quest-name';
+    name.textContent = quest.name;
+
+    const reward = document.createElement('div');
+    reward.className = 'quest-reward';
+    reward.textContent = `Награда: ${quest.reward} монет`;
+
+    const completeBtn = document.createElement('button');
+    completeBtn.className = 'complete-btn';
+    completeBtn.textContent = quest.completed ? 'Выполнено' : 'Выполнить';
+    completeBtn.disabled = quest.completed;
+    completeBtn.onclick = () => {
+      if (!quest.completed) {
+        quest.completed = true;
+        playerBalance += quest.reward;
+        alert(`Квест "${quest.name}" выполнен! Получено ${quest.reward} монет.`);
+        updateHUD();
+        completeBtn.textContent = 'Выполнено';
+        completeBtn.disabled = true;
+      }
+    };
+
+    questCard.appendChild(name);
+    questCard.appendChild(reward);
+    questCard.appendChild(completeBtn);
+
+    container.appendChild(questCard);
   }
 }
 
@@ -698,6 +797,11 @@ function loadLibraries() {
     document.head.appendChild(cannonScript);
   };
   document.head.appendChild(gltfScript);
+
+  // OrbitControls
+  const orbitScript = document.createElement('script');
+  orbitScript.src = 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/controls/OrbitControls.js';
+  document.head.appendChild(orbitScript);
 }
 
 document.addEventListener('DOMContentLoaded', loadLibraries);
